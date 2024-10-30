d5bbf101f0d9a5f5539c70796b32382dd5f950ec
Victor-Borges-Silva <victorborges975@gmail.com>
2024-10-30 14:36:35 -0300

    Criando action

--- determina_ambiente.sh
+++ determina_ambiente.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-set -e
 
-echo "Determining environment based on branch: ${GITHUB_REF}"
+echo "Determinando environment baseado na branch: ${GITHUB_REF}"
 if [[ "${GITHUB_REF}" == "refs/heads/main" ]]; then
-    echo "ambiente=prd" >> $GITHUB_ENV
-    echo "::set-output name=ambiente::prd"
-    echo "Selected environment: prd"
+  echo "ambiente=prd" >> $GITHUB_ENV
+  echo "ambiente=prd" >> $GITHUB_OUTPUT
 elif [[ "${GITHUB_REF}" == "refs/heads/hml" ]]; then
-    echo "ambiente=hml" >> $GITHUB_ENV
-    echo "::set-output name=ambiente::hml"
-    echo "Selected environment: hml"
+  echo "ambiente=hml" >> $GITHUB_ENV
+  echo "ambiente=hml" >> $GITHUB_OUTPUT
 elif [[ "${GITHUB_REF}" == "refs/heads/dev" ]]; then
-    echo "ambiente=dev" >> $GITHUB_ENV
-    echo "::set-output name=ambiente::dev"
-    echo "Selected environment: dev"
-elif [[ "${GITHUB_REF}" == "refs/heads/CHD-13060MelhorarPipelineHML" ]]; then
-    echo "ambiente=hml-teste" >> $GITHUB_ENV
-    echo "::set-output name=ambiente::hml-teste"
-    echo "Selected environment: hml-teste"
+  echo "ambiente=dev" >> $GITHUB_ENV
+  echo "ambiente=dev" >> $GITHUB_OUTPUT
 else
-    echo "ambiente=hml" >> $GITHUB_ENV
-    echo "::set-output name=ambiente::hml"
-    echo "Selected environment: hml"
+  echo "ambiente=hml" >> $GITHUB_ENV
+  echo "ambiente=hml" >> $GITHUB_OUTPUT
 fi
